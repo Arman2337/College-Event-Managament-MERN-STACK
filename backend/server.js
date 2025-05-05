7a894ec950ea4cab4773e7cc6e3f948497f8805e
Arman2337 <23it119@charusat.edu.in>
2025-05-05 11:30:27 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,22 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 
-// Load env vars
 dotenv.config();
 
-// Connect to database
+
 connectDB();
 
 const app = express();
 
-// Body parser
+
 app.use(express.json());
 
-// Enable CORS
 app.use(cors());
 
-// Set static folder
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Mount routers
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
